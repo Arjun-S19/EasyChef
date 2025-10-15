9009807c51426cd86acf83ecea92d3b3a0637edd
Bona T. O'Quinn <bonatoquinn@gmail.com>
2025-10-15 16:05:24 -0700

    Basic API implementation
    -dependency added for internet connection 
    - thread created for API endpoint which has request GET method

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,17 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+<<<<<<< HEAD
+}
+
+android {
+
+    buildFeatures {
+        viewBinding = true
+
+    }
+=======
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
+>>>>>>> 2adbb52ebf87dfede19d67b9789c30621982a91e
 }
